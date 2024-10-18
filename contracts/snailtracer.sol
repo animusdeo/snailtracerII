27eda1c095f6ea9c4d71e6fe0632a200abd7d835
animusdeo <pieter.john.schumacher@gmail.com>
2024-10-18 16:06:37 +0200

    WIP

--- contracts/snailtracer.sol
+++ contracts/snailtracer.sol
@@ -7,18 +7,17 @@ import "./lib/Math.sol";
 contract SnailTracer {
     using Vector3D for Vector3D.Vector;
 
-    int128 immutable width = 1280 * 1e6;
-    int128 immutable height = 720 * 1e6;
-    int128[] buffer;
+    int128 constant h_res = 1280 * 1e6;
+    int128 constant v_res = 720 * 1e6;
 
-    Ray camera;
+    Camera camera;
     Vector3D.Vector deltaX;
     Vector3D.Vector deltaY;
     Triangle[] triangles;
 
     constructor() {
         // Initialize the rendering parameters
-        camera = Ray(
+        camera = Camera(
             Vector3D.Vector(50000000, 52000000, 295600000), 
             Vector3D.Vector(0, -42612, -1000000).norm(), 
             0, 
@@ -42,46 +41,45 @@ contract SnailTracer {
 
     event Pixel(int128, int128, int128);
 
-    function render(int128 spp) public {
-        for (int128 y = height - 1; y >= 0; y--) {
-            for (int128 x = 0; x < width; x++) {
-                Vector3D.Vector memory color = trace(x, y, spp);
+    function render(int128 spp) external {
+        int128 N = spp**2;
+        pigment = Vector3D.Vector(1, 0.98, 0.94);   // default pigment
+        // loop over the screen
+        for (int128 r = h_res - 1; y >= 0; y--) {   // r = row
+            for (int128 c = 0; x < v_res; x++) {    // c = column
+                // https://en.wikipedia.org/wiki/Radiance
+                L = Vector3D.Vector(0, 0, 0);       // SI unit in Joules (J)
+                // for each super sample
+                for (int128 k = 0; k < spp; k++) {
+                    for (int128 l = 0; l < spp; l++) {
+                        // shoot a ray and add the outgoing radiance to the total radiant flux emitted
+                        L.add(Lo(shoot_ray(camera, x, y)));
+                    }
+                }
+                // color == L * pigment / N (in range [0, 255])
+                Vector3D.Vector memory color = pigment.mul(L).div(N).clamp().mul(255).div(1e6);
+                // log pixels using events
                 emit Pixel(color.x, color.y, color.z);
             }
         }
     }
 
-    function trace(int128 x, int128 y, int128 spp) internal view returns (Vector3D.Vector memory color) {
+    function shoot_ray(Camera camera, int128 x, int128 y, int128 spp) internal view returns (Vector3D.Vector memory color) {
         uint32 localSeed = uint32(uint256(int256(y)) * uint256(int256(width)) + uint256(int256(x)));
-
-        for (int128 k = 0; k < spp; k++) {
-            localSeed = nextRand(localSeed);
-            Vector3D.Vector memory pixel = calculatePixel(x, y, localSeed);
-            Ray memory ray = Ray(
-                camera.origin.add(pixel.mul(140)),
-                pixel.norm(),
-                0,
-                false
-            );
-
-            color = color.add(radiance(ray, localSeed));
-        }
-        return color.div(spp**2).clamp().mul(255).div(1e6);
-    }
-
-    function calculatePixel(int128 x, int128 y, uint32 seed) internal view returns (Vector3D.Vector memory) {
-        int128 randX = rand(seed);
-        seed = nextRand(seed);
-        int128 randY = rand(seed);
-
-        Vector3D.Vector memory pixel = camera.direction.add(
-            deltaX.mul((x * 1e6 + randX) / width - 500000).add(
-                deltaY.mul((y * 1e6 + randY) / height - 500000)
-            )
+        localSeed = nextRand(localSeed);
+        Vector3D.Vector memory pixel = camera.dir.add(
+            deltaX.mul((x * 1e6 + randX) / width - 500000)
+            .add(deltaY.mul((y * 1e6 + randY) / height - 500000))
         );
-
-        return pixel;
+        Ray memory ray = Ray(
+            camera.origin.add(pixel.mul(140)),
+            pixel.norm(),
+            0,
+            false
+        );
+        return ray;
     }
+
     function radiance(Ray memory ray, uint32 seed) internal view returns (Vector3D.Vector memory) {
         if (ray.depth > 10) {
             return Vector3D.Vector(0, 0, 0);
@@ -131,7 +129,7 @@ contract SnailTracer {
         return (dist, p, id);
     }
 
-    function radiance(Ray memory ray, Triangle memory obj, int128 dist, uint32 seed) internal view returns (Vector3D.Vector memory) {
+    function Lo(Ray memory ray, Triangle memory obj, int128 dist, uint32 seed) internal view returns (Vector3D.Vector memory) {
         Vector3D.Vector memory intersection = ray.origin.add(ray.direction.mul(dist).div(1e6));
         Vector3D.Vector memory normal = intersection.sub(obj.a).norm();
 
@@ -176,25 +174,24 @@ contract SnailTracer {
         if (det > -1e3 && det < 1e3) {
             return 0;
         }
-        // Calculate and test the 'u' parameter
         Vector3D.Vector memory d = r.origin.sub(t.a);
 
         int128 u = d.dot(p) / det;
         if (u < 0 || u > 1e6) {
             return 0;
         }
-        // Calculate and test the 'v' parameter
         Vector3D.Vector memory q = d.cross(e1);
 
         int128 v = r.direction.dot(q) / det;
         if (v < 0 || u + v > 1e6) {
             return 0;
         }
-        // Calculate and return the distance
+
         int128 dist = e2.dot(q) / det;
         if (dist < 1e3) {
             return 0;
         }
+
         return dist;
     }
 
@@ -218,12 +215,35 @@ contract SnailTracer {
         }
         return 0;
     }
+
+    function intersect(AreaLight memory a, Ray memory r) internal pure returns (int128) {
+        int128 t = a.p.min(r.x).dot(a.n) / r.wo.dot(a.n);
+        if (t <= r.t_min || r.t_max < t) {
+            return 0;
+        }
+
+        Vector3D.Vector memory d = r.x + r.wo.min(a.p).mul(t);
+        
+        int128 t1 = d.dot(a.u);
+        if (t1 < 0 || a.u.norm().mul(a.u.norm()) < t1) { // implement .squaredNorm() for vectors
+            return 0;
+        }
+
+        int128 t2 = d.dot(a.v);
+        if (t2 < 0 || a.v.norm().mul(a.v.norm()) < t2) {
+            return 0;
+        }
+
+        r.t_max = t;
+        return r.t_max - r.t_min;
+    }
     
     struct Ray {
-        Vector3D.Vector origin;
-        Vector3D.Vector direction;
-        int128          depth;
-        bool            refract;
+        Vector3D.Vector x;      // origin
+        Vector3D.Vector wo;     // direction
+        int128          t_min;  // minimum distance of intersection
+        int128          t_max;  // maximum distance of intersection
+        int128          d;      // recursion depth
     }
 
     enum Material { Diffuse, Specular }
@@ -243,6 +263,27 @@ contract SnailTracer {
         Vector3D.Vector emission;
         Vector3D.Vector color;
         Material        reflection;
+    }
+
+    //    p2  --                  --
+    //      |    |      == >    |    |
+    //      |    |      == >  v |    |
+    //    p1  --  p3            p --
+    //                            u
+    // we define rectangles as such: local 2D orthonormal basis u, v with origin p (most negative point i.e. lowerleft corner)
+    struct AreaLight {
+        int128          Le;     // Light emitted = color * intensity * diffuse material coeff / area 
+        Vector3D.Vector n;      // normal
+        Vector3D.Vector p;      // basis vector origin
+        Vector3D.Vector u;      // basis vector x
+        Vector3D.Vector v;      // basis vector y
+    }
+
+    struct Camera {
+        Vector3D.Vector eye;    // eye
+        Vector3D.Vector dir;    // looks at
+        int128          dist;   // plane offset i.e. distance from the eye to the screen
+     }
     }
 
     function rand(uint32 seed) internal pure returns (int128) {
